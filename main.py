8eddef48c09dbf29851349ac40450cd6d9b0708e
Dhananjay_07 <mandlapured@gmail.com>
2025-05-21 22:23:58 +0530

    Base_model Prepared

--- main.py
+++ main.py
@@ -7,10 +7,10 @@ STAGE_NAME = "Data Ingestion stage"
 
 
 try:
-    config = ConfigurationManager()
-    data_ingestion_config = config.get_data_ingestion_config()
-    data_ingestion = DataIngestion(config=data_ingestion_config)
-    data_ingestion.download_file()
-    data_ingestion.extract_zip_file()
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
+    logger.exception(e)
     raise e
